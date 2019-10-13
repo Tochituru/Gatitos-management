6aa1835e9a66e712f272cb8702f3c58b5a40ed84
Tochituru <rotrupo@gmail.com>
2019-10-13 01:46:34 -0300

    things to do list (and first text test)

--- public/dom.js
+++ public/dom.js
@@ -1,1 +1,15 @@
+//crear las filas
+//crear celdas
+//crear botones editar borrar
+//hacer el fetch de get 
+//hacer modal para el post 
+//hacer el post
+//hacer modal para el patch
+//hacer el patch 
+//hacer modal de delete
+//hacer delete 
+//hacer el campo de busqueda
+// hacer el filter
+// hacer las validaciones 
+ 
 console.log('funciona(?')
