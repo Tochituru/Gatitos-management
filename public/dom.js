ace7b31ecdd3e5cadadaa7429d689fcc6291b4c5
Tochituru <rotrupo@gmail.com>
2019-10-13 13:18:43 -0300

    get is working

--- public/dom.js
+++ public/dom.js
@@ -1,7 +1,40 @@
+const api = 'http://localhost:3000/api/kittens';
+let kittenTable = document.getElementById('kittenTable')
+
 //crear las filas
 //crear celdas
+const createCell = (fieldClass, fieldValue) => {
+    let newCell = document.createElement('td');
+    newCell.innerHTML = fieldValue;
+    newCell.classList.add(fieldClass); 
+    console.log('cell', newCell)
+    return newCell 
+}
 //crear botones editar borrar
+
+const createTable = element => {
+element.forEach(element=>{
+    let newRow= document.createElement('tr');
+    newRow.appendChild(createCell('name', element.name));    newRow.appendChild(createCell('adoptionDate', element.adoptionDate));
+    newRow.appendChild(createCell('color', element.color));
+    newRow.appendChild(createCell('favoriteToy', element.favoriteToy));
+    newRow.appendChild(createCell('email', element.email));
+
+    console.log(newRow);
+    
+    return kittenTable.appendChild(newRow);
+})
+}
 //hacer el fetch de get 
+
+const initialize = () => {
+    fetch(api)
+        .then(res => res.json())
+        .then(kittens => createTable(kittens.kittens))
+
+        };
+
+
 //hacer modal para el post 
 //hacer el post
 //hacer modal para el patch
@@ -11,5 +44,3 @@
 //hacer el campo de busqueda
 // hacer el filter
 // hacer las validaciones 
- 
-console.log('funciona(?')
