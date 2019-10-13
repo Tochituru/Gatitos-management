bc793dc39263be0f9037e6814b03d0a120a5f18b
Tochituru <rotrupo@gmail.com>
2019-10-12 22:15:42 -0300

    started get and patch (seem to work)

--- api/kittens.js
+++ api/kittens.js
@@ -1,5 +1,5 @@
 const kittens = [
-    {id:'1',name:'Benja', adoptionDate: '12-10-2004', color: 'gris y verde', favoriteToy: 'Fede', email: 'benja@muerdomucho.com'},
+    {id:'1',name:'Benjamonja', adoptionDate: '12-10-2004', color: 'gris y verde', favoriteToy: 'Fede', email: 'benja@muerdomucho.com'},
     {id:'2',name:'Benja', adoptionDate: '12-10-2004', color: 'gris y verde', favoriteToy: 'Fede', email: 'benja@muerdomucho.com'},
     {id:'3',name:'Benja', adoptionDate: '12-10-2004', color: 'gris y verde', favoriteToy: 'Fede', email: 'benja@muerdomucho.com'},
     {id:'4',name:'Benja', adoptionDate: '12-10-2004', color: 'gris y verde', favoriteToy: 'Fede', email: 'benja@muerdomucho.com'},
@@ -7,15 +7,23 @@ const kittens = [
 ]
 
 const getKittens = (req, res, next) => {
-    res
-    console.log(kittens)
-}
+    res.status(200).json({ kittens });
+    next();
+    console.log('se hizo el get kittens')
+};
+    
 const postKitten = (req, res, next) => {
     console.log(kittens);
     
 }
 const patchKitten = (req, res, next) => {
-    console.log(kittens)
+    const kittenId= req.params.id;
+    const kitten = kittens.filter(kitten => {
+        return kitten.id === kittenId
+    })[0]
+    res.status(200).json(kitten);
+    next();
+    console.log('se trata de hacer un patch con : ' , kitten)
 }
 const deleteKitten = (req, res, next) => {
     console.log(kittens)
